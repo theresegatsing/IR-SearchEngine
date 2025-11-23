8d4c40f059302b7266fe05e6f7bcf9b1ce2fa8d0
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-22 20:01:45 -0500

    Merge branch 'main' of https://github.com/theresegatsing/Search_Engine into main

--- src/searchengine/SearchResult.java
+++ src/searchengine/SearchResult.java
@@ -1,5 +1,15 @@
 package searchengine;
 
-public class SearchResult {
+
+/**
+ * Represents the result of a search:
+ *   - which document matched
+ *   - the score (relevance)
+ *   - a snippet to show to the user
+ *
+ * Implements Comparable so we can sort by score descending.
+ */
+
+public class SearchResult implements Comparable<SearchResult> {
 
 }
