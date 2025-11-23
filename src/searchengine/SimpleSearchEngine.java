146cf48165a76a6ecb2fdfe231c73b5ff0bd7fd0
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-22 20:22:21 -0500

    Initialised the SimpleSearchEngine class

--- src/searchengine/SimpleSearchEngine.java
+++ src/searchengine/SimpleSearchEngine.java
@@ -1,5 +1,15 @@
 package searchengine;
 
+import java.util.*;
+
+/**
+ * High-level search engine:
+ *   - holds documents
+ *   - builds an inverted index
+ *   - takes user queries, uses BooleanQueryProcessor
+ *   - scores and sorts results
+ */
+
 public class SimpleSearchEngine {
 
 }
