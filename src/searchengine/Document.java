78084d894ae1e915223efece237894cf7cddfc9e
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-11-22 19:43:13 -0500

    Initialized java project and classes

---
+++ src/searchengine/Document.java
@@ -0,0 +1,16 @@
+package searchengine;
+
+import java.time.LocalDate;
+
+/**
+ * Represents a single document that can be searched.
+ * Each document has:
+ *   - an id (int)
+ *   - a title
+ *   - full text content
+ *   - a date (for sorting by date)
+ */
+
+public class Document {
+
+}
